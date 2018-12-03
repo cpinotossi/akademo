b9c0107cfc06ab4a10b728cbb914b0d2b20c7075
cpinotos <cpinotos@akamai.com>
2018-12-03 01:14:13 +0100

    generate gallery dynamic

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,37 @@ var router = express.Router();
 var request = require('request');
 var parser = require('xml2json');
 
-/* GET home page. */
-router.get('/', function(req, res, next){
-    // Retrieve current list of Images
-    request('http://api.hebe.io/img/cpt', function (error, response, body) {
-          console.log('error:', error); // Print the error if one occurred and handle it
-          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          var imageArray = null;
-          if(response.statusCode == 200){
-            // Transform into an json object
-            fileList = (parser.toJson(body, {object: true})).stat.file;
-            //console.log('body:'+body);
-            console.log('fileList:'+ JSON.stringify(fileList, null, 2));
-            // Iterate through the json object file entries and generate corresponding html code.
-            imageArray = [];
-            for (var file in fileList) {
-              if((fileList[file].name).endsWith(".jpg")){
-                console.log("file.name: "+fileList[file].name);
-                imageArray.push(fileList[file].name);
-              }
-            }
-          }
-          //render result
-          res.render('index', { title: 'Christian', imageArray: imageArray });
-    });
+var userName = "Christian";
+var policyImageManager = "thumbnails";
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // Retrieve current list of Images via NetStorage HTTP API
+  request('http://api.hebe.io/img/cpt', function(error, response, body) {
+    console.log('Netstorage File List statusCode:', response && response.statusCode);
+    //Define array in which we will store all the file names which we recieve from NetStorage HTTP API
+    var imageArray = null;
+    //Execute only if we receive an 200 from NetStorage HTTP API
+    if (response.statusCode == 200) {
+      // Transform NetStorage File list from xml to json object
+      var fileArray = (parser.toJson(body, {
+        object: true
+      })).stat.file;
+      // Iterate through the json object file entries and generate corresponding html code.
+      imageArray = [];
+      for (var file in fileArray) {
+        if ((fileArray[file].name).endsWith(".jpg")) {
+          imageArray.push(fileArray[file].name);
+        }
+      }
+    }
+    //render result
+    res.render('index', {
+      title: userName,
+      pim: policyImageManager,
+      imageArray: imageArray
+    });
+  });
 })
 
 
