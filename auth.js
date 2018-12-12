ebdbacbdce008da96dd1f3e51ca6b3b28535dd98
cpinotos <cpinotos@akamai.com>
2018-12-12 14:11:15 +0100

    new auth.js

---
+++ auth.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+module.exports = (passport) => {
+  // Used to stuff a piece of information into a cookie
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  // Used to decode the received cookie and persist session
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.use(new GoogleStrategy({
+      clientID: '1052562236333-agfmaqsb5bg5310n9n4l2cnt0ksefg8d.apps.googleusercontent.com',
+      clientSecret: process.env['googleClientSecret'],
+      callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    (token, refreshToken, profile, done) => {
+      // passes the profile and token data to serializeUser
+      return done(null, {
+        profile: profile,
+        token: token
+      });
+    }));
+};
+
+function extractProfile(profile) {
+  let imageUrl = '';
+  if (profile.photos && profile.photos.length) {
+    imageUrl = profile.photos[0].value;
+  }
+  return {
+    id: profile.id,
+    displayName: profile.displayName,
+    image: imageUrl
+  };
+}
